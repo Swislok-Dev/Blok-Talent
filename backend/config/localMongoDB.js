baac3031c95a5ec9cb2f827f1ace9568c667833b
Swislok-Dev <cmarker4@gmail.com>
2022-06-29 22:52:47 -0500

    log connection url

--- backend/config/localMongoDB.js
+++ backend/config/localMongoDB.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const connectLocalDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.LOCAL_MONGO_URI)
+    const db = conn.connection
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB connected on ${db.host}:${db.port}/${db.name}`.cyan.underline)
   } catch (error) {
     console.log(error, 'Error occurred in the backend/config/localMongoDB.js')
     process.exit(1)
