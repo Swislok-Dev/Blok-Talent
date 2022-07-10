91a5352e4daff32232ec7aa56ef3d75bfae9f0a3
Swislok-Dev <cmarker4@gmail.com>
2022-07-10 10:07:08 -0500

    template homepage

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Blok Talent</h1>
+      <p>This is a project management application for all you needs</p>
+    </div>
+  )
+}
+
+export default Home
