42ff50d2a0e4f477f6b2026f1b6bc0d19503ce68
Swislok-Dev <cmarker4@gmail.com>
2022-07-16 12:35:49 -0500

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
 function App() {
   return (
@@ -11,8 +15,12 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login /> } />
+          <Route path='/register' element={<Register /> } />
+
         </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </>
   )
 }
